4c71d55214c465a54cb001358ab29430059af44f
leejay <buttercookie210@gmail.com>
2025-10-02 23:57:55 +0100

    refactor: change middleware setting method

--- app-server/app/main.py
+++ app-server/app/main.py
@@ -1,24 +1,32 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers.wishlist import router as wishlist_router
 
 app = FastAPI()
 
 app.include_router(wishlist_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.middleware("http")
-async def add_cors_headers(request, call_next):
-    response = await call_next(request)
-
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-
-    if request.method == "OPTIONS":
-        response.status_code = 200
-
-    return response
+# @app.middleware("http")
+# async def add_cors_headers(request, call_next):
+#     response = await call_next(request)
+#
+#     response.headers["Access-Control-Allow-Origin"] = "*"
+#     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+#     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+#     response.headers["Access-Control-Allow-Credentials"] = "true"
+#
+#     if request.method == "OPTIONS":
+#         response.status_code = 200
+#
+#     return response
 
 
 @app.get("/")
